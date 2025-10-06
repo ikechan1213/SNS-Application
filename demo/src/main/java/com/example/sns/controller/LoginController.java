29e3c24291561fe68086ecb2ef6e02b3cec24ce4
IKE1213 <ikedakoukun1213@gmail.com>
2025-10-06 10:56:54 +0900

    deploy成功！「/login」リダイレクト追加

--- demo/src/main/java/com/example/sns/controller/LoginController.java
+++ demo/src/main/java/com/example/sns/controller/LoginController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-//    @GetMapping("/")
-//    public String root() {
-//        // ルートURL("/")にアクセスされたら"/login"にリダイレクトする
-//        return "redirect:/login";
-//    }
-//
+    @GetMapping("/")
+    public String root() {
+        // ルートURL("/")にアクセスされたら"/login"にリダイレクトする
+        return "redirect:/login";
+    }
+
 
     @GetMapping("/login")
     public String showLoginForm() {
